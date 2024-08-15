966d02eb4b16d8239fdc904f25c6acc533c46ea8
New2024-Deve <ayaezzat2289@gmail.comm>
2024-08-15 16:07:51 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,30 @@ function App() {
   
   return (
     <>
-      <h1>Hallo aya</h1>
+      <div id='main'>
+        <h1>Contact Us</h1>
+        <label>First Name</label>
+        <input type="text" />
+        <label>Last Name</label>
+        <input type="text" />
+        <p>Email Adress</p>
+        <input type="text" />
+        <p>Query Type</p>
+        <input type="checkbox" />
+        <label htmlFor="">General Enquiry</label>
+        <input type="checkbox" />
+        <label htmlFor="">Support Request</label>
+        <p>Message</p>
+        <textarea></textarea>
+        <div>
+        <input type="checkbox" />
+        <label htmlFor="">I consent to being contacted by the team</label>
+        </div>
+        <button>Submit</button>
+
+        
+
+      </div>
     </>
   )
 }
