6afd2854c7baba9300e7de2aa6adba0e8782f4ef
New2024-Deve <ayaezzat2289@gmail.comm>
2024-08-15 20:19:11 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,96 @@
 
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+const [formDate, setformDate] = useState({
+  firstName:"", lastName:"", email:"", Query:"",
+  message:"", ruleAccepted: false,
+  
+});
+
+  function Formonsubmit(event){
+    event.preventDefault();
+    console.log(formDate)
+  };
+
+  function Formonchange(event){
+    const keyname = event.target.name;
+    var keyvalue = event.target.value;
+    const type = event.target.type;
+    
+
+    if(type == "checkbox"){
+      keyvalue = event.target.checked;
+    }
+
+    setformDate({
+      ...formDate,
+      [keyname]: keyvalue
+    })
+    }
+    
   
   return (
     <>
-      <div id='main'>
+      <form id='main' onSubmit={Formonsubmit}>
         <h1>Contact Us</h1>
-        <label>First Name</label>
-        <input type="text" />
-        <label>Last Name</label>
-        <input type="text" />
-        <p>Email Adress</p>
-        <input type="text" />
-        <p>Query Type</p>
-        <input type="checkbox" />
-        <label htmlFor="">General Enquiry</label>
-        <input type="checkbox" />
-        <label htmlFor="">Support Request</label>
-        <p>Message</p>
-        <textarea></textarea>
+
+    <div id='first-last'>
+        <div id='first'>
+        <label htmlFor='firstName'>First Name</label>
+        <input id='firstName' name='firstName' type="text" 
+        value={formDate.firstName} onChange={Formonchange}/>
+        </div>
+
+        <div id='last'>
+        <label htmlFor='lastName'>Last Name</label>
+        <input id='lastName' name='lastName' type="text" 
+        value={formDate.lastName} onChange={Formonchange}/>
+        </div>
+        </div>
+
+        <div  id='email'>
+        <label htmlFor='email'>Email Adress</label>
+        <input id='email' name='email' type="email" 
+        value={formDate.email} onChange={Formonchange}/>
+        </div>
+
+       
+        <p id='Query'>Query Type</p>
+        <div id='radidiv'>
+        <div id='General'>
+        <input name='Query' type="radio" 
+        value="General" onChange={Formonchange}/>
+        <label htmlFor="General">General Enquiry</label>
+        </div>
+
+        <div id='Support'>
+        <input name='Query' type="radio" 
+        value="Support" onChange={Formonchange}/>
+        <label htmlFor="Support">Support Request</label>
+        </div>
+
+        </div>
+
+        <div id='messdiv'>
+        <label htmlFor='message'>Message</label>
+        <textarea name='message' id='message'
+        value={formDate.message} onChange={Formonchange}></textarea>
+        </div>
+
         <div>
-        <input type="checkbox" />
+        <input type="checkbox" name='ruleAccepted'
+        onChange={Formonchange} checked={formDate.ruleAccepted}/>
         <label htmlFor="">I consent to being contacted by the team</label>
         </div>
-        <button>Submit</button>
+
+        <button type="submit" id='sub'
+        disabled={formDate.ruleAccepted? false: true}>Submit</button>
 
         
 
-      </div>
+      </form>
     </>
   )
 }
